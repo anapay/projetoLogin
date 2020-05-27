4f4800a15989470d2546c26a2d8f39fa8970897b
anapay <anapay42@gmail.com>
2020-05-27 11:53:24 -0300

    Página inicial do login

---
+++ login.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div id="corpo-form">
+        <h1>Entrar</h1>
+        <form method="POST">
+        <input type="email" placeholder="Usuário" name="email">
+        <input type="password" placeholder="Senha" name="senha">
+        <input type="submit" value="ACESSAR">
+        <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se</strong></a>
+        </form>
+    </div>
+    <?php
+
+
+    ?>
+</body>
+</html>
